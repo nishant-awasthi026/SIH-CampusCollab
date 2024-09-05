5e207f43a6f0d212931f2ff85e6075fbfa9ed74d
Nishant Awasthi <113894715+nishant-awasthi026@users.noreply.github.com>
2024-09-05 15:08:19 +0530

    Update Stripes.jsx

--- src/components/Stripes.jsx
+++ src/components/Stripes.jsx
@@ -4,7 +4,7 @@ import Stripe from "./Stripe";
 const Stripes = () => {
   const data = [
     {
-      url: "https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63c9ce90a51cd7cf6b3689fb_css-design-awards-logos-id1L9L8Yvp%201.svg",
+      <h2>CLUBS</h2>,
       number: 52,
     },
     {
